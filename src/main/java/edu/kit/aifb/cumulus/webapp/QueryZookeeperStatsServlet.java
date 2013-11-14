364d98dcbed3678c03b464e40377127b93329d70
Teodor Macicas <teodor@exascale.info>
2013-11-14 11:55:00 +0100

    bug fix

--- src/main/java/edu/kit/aifb/cumulus/webapp/QueryZookeeperStatsServlet.java
+++ src/main/java/edu/kit/aifb/cumulus/webapp/QueryZookeeperStatsServlet.java
@@ -1,10 +1,6 @@
 package edu.kit.aifb.cumulus.webapp;
 
-import edu.kit.aifb.cumulus.store.AbstractCassandraRdfHector;
-import edu.kit.aifb.cumulus.store.Store;
-import edu.kit.aifb.cumulus.store.StoreException;
 import java.io.IOException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletContext;
@@ -15,11 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.semanticweb.yars.nx.Node;
 
 import edu.kit.aifb.cumulus.webapp.formatter.SerializationFormat;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-import me.prettyprint.hector.api.beans.HColumn;
 import org.apache.curator.framework.recipes.locks.InterProcessReadWriteLock;
 import org.semanticweb.yars.nx.Variable;
 import org.semanticweb.yars.nx.parser.NxParser;
@@ -43,7 +36,6 @@ public class QueryZookeeperStatsServlet extends AbstractHttpServlet {
 			sendError(ctx, req, resp, HttpServletResponse.SC_NOT_ACCEPTABLE, "no known mime type in Accept header");
 			return;
 		}
-		int queryLimit = (Integer)ctx.getAttribute(Listener.QUERY_LIMIT);
 		resp.setCharacterEncoding("UTF-8");
                 // header needed to allow local javascript http requests
                 resp.addHeader("Access-Control-Allow-Origin", "*");
@@ -51,6 +43,7 @@ public class QueryZookeeperStatsServlet extends AbstractHttpServlet {
                 // default no of trials
                 int trials_int = 100;
 		String trials = req.getParameter("trials");
+                String id = req.getParameter("id");
                 if( trials == null || trials.isEmpty() ) {
                     trials_int = Integer.parseInt(trials);
                 }
@@ -63,9 +56,13 @@ public class QueryZookeeperStatsServlet extends AbstractHttpServlet {
                 long total_acquire_write_time=0;
                 long total_release_write_time=0;
                 long start_time;
-                
+
+                if( id == null || id.isEmpty() ) {
+                    id = UUID.randomUUID().toString();
+                }
+
                 for( int i=0; i<trials_int; ++i ) {
-                    String key_e = "eeeeeeeeeeeeeeeeeeee"+i;
+                    String key_e = "eeeeeeeeeeeeeeeeeeee"+i+"-"+id;
                     rw_lock = new InterProcessReadWriteLock(Listener.curator_client, "/e/"+key_e);
                     start_time = System.currentTimeMillis();
                     // acquire and release a read lock here
